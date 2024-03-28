a4ab2bd11e46132f6976fea51a9cd8b423974ca5
Wes Shaddix <wes.shaddix@outlook.com>
2024-03-28 12:34:06 -0400

    updating wezterm preferences

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,11 +5,11 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'nord'
 
 -- Set the font to use in the terminal
-config.font = wezterm.font 'Inconsolata Nerd Font'
-config.font_size = 15
+config.font = wezterm.font 'MonoLisa'
+config.font_size = 12
 
 -- Custom Key Bindings
 config.keys = {
