32ba7e1b4acde2d96946204f54e1366ab8a70e90
Wes Shaddix <wes.shaddix@outlook.com>
2024-05-29 21:27:01 -0400

    Update run_once_04_configure_settings.sh

--- run_once_04_configure_settings.sh
+++ run_once_04_configure_settings.sh
@@ -1,4 +1,4 @@
-#! bin/sh
+#!/bin/sh
 
 # Prevent Crackling with PipeWire/WirePlumber
 sudo sed -i 's/--\["session.suspend-timeout-seconds"\] = 5/\["session.suspend-timeout-seconds"\] = 0/' /usr/share/wireplumber/main.lua.d/50-alsa-config.lua
