8f0bf21d7f0186bb296190416c9ebfb9de56ebce
Wes Shaddix <wes.shaddix@outlook.com>
2024-10-11 20:52:52 -0400

    Update run_once_00_install_package_repos.sh

--- run_once_00_install_package_repos.sh
+++ run_once_00_install_package_repos.sh
@@ -29,8 +29,8 @@ sudo add-apt-repository 'deb https://typora.io/linux ./'
 # sudo apt-add-repository -y ppa:system76-dev/stable
 
 # sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+# echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 # update the repository list
 sudo apt-get update
