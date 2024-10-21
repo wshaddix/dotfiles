351e772b654eaf2416efc0e62c7e3356c3c2d899
Wes Shaddix <wshaddix@wshaddix-laptop.lan>
2024-10-21 09:42:17 -0400

    updating configs

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,23 +1,21 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 
 -- Set the font to use in the terminal
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 13
+config.font = wezterm.font("Inconsolata Nerd Font")
+config.font_size = 14
 
 config.enable_tab_bar = false
 -- config.window_decorations = "RESIZE"
 
 -- Custom Key Bindings
-config.keys = {
-
-}
+config.keys = {}
 
 config.window_background_opacity = 0.95
 
