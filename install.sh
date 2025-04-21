a9fa65a9a8f15a6e37884f65e911aaf898e91102
Wes Shaddix <wes.shaddix@outlook.com>
2025-04-21 12:16:30 -0400

    Update install.sh
    Adding some error handling

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Give people a chance to retry running the installation
+trap 'echo "Installation failed! You can retry by running: source ~/.local/share/wshaddix-ubuntu/install.sh"' ERR
+
 echo -e "\nInstallation coming soon!"
