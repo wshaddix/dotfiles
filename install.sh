489ac7a3a6869129a584445b0c2a73c86684a9ec
Wes Shaddix <wes.shaddix@outlook.com>
2025-04-21 13:05:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo -e "\nInstalling Ghostty terminal"
+echo "Installing Ghostty terminal"
 cd ~/Downloads
 GHOSTTY_URL="https://github.com/mkasberg/ghostty-ubuntu/releases/download/1.1.3-0-ppa2/ghostty_1.1.3-0.ppa2_amd64_25.04.deb"
 curl -sLo ghostty.deb "${GHOSTTY_URL}"
