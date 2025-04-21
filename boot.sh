f022612623f5dc70f7b5e2b1ff06281e5115563e
Wes Shaddix <wes.shaddix@outlook.com>
2025-04-21 13:15:16 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -36,7 +36,8 @@ else
     echo "Adding Brave keyring and package source"
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt-get update
 fi
+
+sudo apt-get update
 
 sudo apt-get install brave-browser -y
