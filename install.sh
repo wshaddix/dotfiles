96aeb5b6f59f5aa6df0671783805eab67fab5d73
Wes Shaddix <wes.shaddix@outlook.com>
2025-04-21 12:30:25 -0400

    Update install.sh
    Adding Ghostty terminal installation

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ set -e
 # Give people a chance to retry running the installation
 trap 'echo "Installation failed! You can retry by running: source ~/.local/share/wshaddix-ubuntu/install.sh"' ERR
 
-echo -e "\nInstallation coming soon!"
+echo -e "\nInstalling Ghostty terminal"
+cd ~/Downloads
+GHOSTTY_VERSION=$(curl -s "https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest" | grep -Po '"tag_name": "\d.\d.\d.+')
+curl -sLo lazygit.tar.gz "https://github.com/mkasberg/ghostty-ubuntu/releases/latest/download/ghostty_${GHOSTTY_VERSION}_amd64_25.04.deb"
+sudo apt-get install -y ./ghostty_*.deb >/dev/null
